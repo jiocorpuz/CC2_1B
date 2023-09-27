2074203b631c562fa2d7caa2a258a67993b47dda
Jron Ivan O. Corpuz <145944382+jiocorpuz@users.noreply.github.com>
2023-09-27 08:47:29 +0800

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+# input the variables
+name = input("What is your name? ")
+age_input = input("What is your age?")
+age = int(age_input)
+age_next_year = age + 1
+
+# Arithmetic Operations
+pi = 3.1416
+modulo = 5 % 3
+
+# prints the variables
+print(f"Hello, {name}")
+print(f"Your age next year is {age_next_year}")
+print(f"Pi value is {pi}")
+print(f"5 modulo 3 is {modulo}")
+print(f"5 times 3 is {5*3}")
