442c67eee4f2834458bd1d0faa4aba96bfeba52b
Jron Ivan O. Corpuz <145944382+jiocorpuz@users.noreply.github.com>
2023-09-28 07:52:45 +0800

    Update helloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,8 +9,16 @@ pi = 3.1416
 modulo = 5 % 3
 
 # prints the variables
+# f = function
 print(f"Hello, {name}")
 print(f"Your age next year is {age_next_year}")
 print(f"Pi value is {pi}")
 print(f"5 modulo 3 is {modulo}")
 print(f"5 times 3 is {5*3}")
+
+print('Hello "friend"')
+print("Hello \"friend\"")
+
+# Lists/Dictionary
+char = {alpha, beta, gamma, zeta}
+print(char[0])
